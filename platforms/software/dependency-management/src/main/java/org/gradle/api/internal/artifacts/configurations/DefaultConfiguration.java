9cde55e05c935b4943da1b576236363573a4e022
Tom Tresansky <ttresansky@gradle.com>
2025-05-12 11:20:50 -0400

    Detached Configurations now use a non-migrating role

--- platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/configurations/DefaultConfiguration.java
+++ platforms/software/dependency-management/src/main/java/org/gradle/api/internal/artifacts/configurations/DefaultConfiguration.java
@@ -1206,7 +1206,7 @@ public abstract class DefaultConfiguration extends AbstractFileCollection implem
             configurationsProvider,
             childResolutionStrategy,
             rootComponentMetadataBuilder,
-            ConfigurationRolesForMigration.LEGACY_TO_RESOLVABLE_DEPENDENCY_SCOPE
+            ConfigurationRoles.RESOLVABLE_DEPENDENCY_SCOPE
         );
         configurationsProvider.setTheOnlyConfiguration(copiedConfiguration);
         return copiedConfiguration;
